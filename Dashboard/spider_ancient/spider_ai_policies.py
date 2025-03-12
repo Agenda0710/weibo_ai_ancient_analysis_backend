1124a5b0d0a42ee22a0562343e0d924c965dde3d
议程 <yichengzhao5@gmail.com>
2025-03-12 22:39:36 +0800

    修改政策分析（爬取非物质文化遗产）

--- Dashboard/spider_ancient/spider_ai_policies.py
+++ Dashboard/spider_ancient/spider_ai_policies.py
@@ -39,7 +39,7 @@ def get_ai_policies_information():
         params = {
             'sortType': 1,
             'p': j,
-            'q': '中国古代科学',
+            'q': '非物质文化遗产',
         }
 
         response = requests.get(url, headers=headers, params=params)
