76bb2a8c02502e7cda1b234248f97e936f1032d2
议程 <yichengzhao5@gmail.com>
2025-03-12 22:38:33 +0800

    修改政策分析

--- Dashboard/spider_ai/spider_ai_policies.py
+++ Dashboard/spider_ai/spider_ai_policies.py
@@ -39,7 +39,7 @@ def get_ai_policies_information():
         params = {
             'sortType': 1,
             'p': j,
-            'q': '非物质文化遗产',
+            'q': '人工智能',
         }
 
         response = requests.get(url, headers=headers, params=params)
